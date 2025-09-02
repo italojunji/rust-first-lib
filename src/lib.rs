36088d73a0979d50ccd3f227d55547a47f2e20ab
Italo Matsushita <italo_junji@yahoo.com.br>
2025-09-02 08:45:07 -0300

    Using rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,41 @@
 pub mod calc_with_1 {
 
-
     ///    # This function sums and then adds plus 1
     ///    ## Example
     ///    ```rust
     ///    use calc_near_x_ijm::calc_with_1::sum_plus_one;
-    /// 
+    ///
     ///    assert_eq!(42, sum_plus_one(41,0));
     ///    assert_eq!(4, sum_plus_one(1,2));
     ///    assert_eq!(1, sum_plus_one(0,0));
-    pub fn sum_plus_one(x: u8, y:u8) -> u8 {
-        return x + y + 1
-
+    pub fn sum_plus_one(x: u8, y: u8) -> u8 {
+        return x + y + 1;
     }
 
     ///    # This function subtracts and then decreases minus 1
     ///     - If x less than or equals to y, returns 0.
-    /// 
+    ///
     ///    ## Example
     ///    ```rust
     ///    use calc_near_x_ijm::calc_with_1::sub_minus_one;
-    /// 
+    ///
     ///    assert_eq!(40, sub_minus_one(41,0));
     ///    assert_eq!(0, sub_minus_one(6,6));
     ///    assert_eq!(0, sub_minus_one(0,50));
-    pub fn sub_minus_one(x: u8, y:u8) -> u8 {
+    pub fn sub_minus_one(x: u8, y: u8) -> u8 {
         if x <= y {
-            return 0
+            return 0;
         }
 
-        return x - y - 1
+        return x - y - 1;
     }
-
-
 }
 
 #[cfg(test)]
 mod test {
 
     use super::calc_with_1;
-    
+
     #[test]
     fn test_sum() {
         let result = calc_with_1::sum_plus_one(5, 6);
@@ -89,5 +85,4 @@ mod test {
 
         assert_eq!(4, result);
     }
-    
 }
